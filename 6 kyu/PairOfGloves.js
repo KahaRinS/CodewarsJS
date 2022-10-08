da1b775605bdabfa7c9dbf8ff7488acbdd96d980
Вячеслав Кахановский <vaceslavkahanovskij@MacBook-Air-Vaceslav.local>
2022-10-08 21:20:07 +0300

    feat: solve Pair of gloves KATA

---
+++ 6 kyu/PairOfGloves.js
@@ -0,0 +1,20 @@
+function numberOfPairs(glove){
+    let gloves = glove.slice()
+    let color = ""
+    let fullCount = 0
+    while (gloves.length!=0){
+        color = gloves[0]
+        let count=0
+        while(gloves.indexOf(color)!=-1) {
+            gloves.splice(gloves.indexOf(color),1)
+            count++
+        }
+        if(count!=1){
+            fullCount=fullCount + (count-count%2)/2
+        }
+    }
+    return fullCount
+}
+
+let p = ['red','green','blue']
+console.log(numberOfPairs(p))
